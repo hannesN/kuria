b58a603597063e8d1f3772014fcf3dac0dc8e010
niederhausen <niederhausen@localhost>
2010-04-06 10:39:28 +0200

    * removed lib folder * minor cleanup

--- kuria.runtime/src/main/java/de/topicmapslab/kuria/runtime/BindingContainer.java
+++ kuria.runtime/src/main/java/de/topicmapslab/kuria/runtime/BindingContainer.java
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-/**
- * 
- */
 package de.topicmapslab.kuria.runtime;
 
 import java.util.Collections;
